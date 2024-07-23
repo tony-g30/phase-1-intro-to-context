a2c41afb7271b8aa52633a157dbfaa952cf42477
tony-g30 <tonyaudo@gmail.com>
2024-07-23 12:32:59 +0300

    corections in lab

--- index.js
+++ index.js
@@ -1,81 +1,62 @@
 // Function to create a single employee record
-function createEmployeeRecord(arr) {
+function createEmployeeRecord([firstName, familyName, title, payPerHour]) {
     return {
-    firstName: arr[0],
-    familyName: arr[1],
-    title: arr[2],
-    payPerHour: arr[3],
+    firstName: firstName,
+    familyName: familyName,
+    title: title,
+    payPerHour: payPerHour,
     timeInEvents: [],
     timeOutEvents: []
     };
     }
     
     // Function to create multiple employee records
-    function createEmployeeRecords(arrays) {
-    return arrays.map(createEmployeeRecord);
+    function createEmployeeRecords(employeeData) {
+    return employeeData.map(data => createEmployeeRecord(data));
     }
     
-    // Function to create a time in event
-    function createTimeInEvent(dateStamp) {
+    // Function to create a time-in event
+    function createTimeInEvent(employeeRecord, dateStamp) {
     let [date, hour] = dateStamp.split(' ');
-    
-    this.timeInEvents.push({
-        type: "TimeIn",
-        hour: parseInt(hour, 10),
-        date: date
+    employeeRecord.timeInEvents.push({
+    type: "TimeIn",
+    hour: parseInt(hour, 10),
+    date: date
     });
-    
-    return this;
+    return employeeRecord;
     }
     
-    // Function to create a time out event
-    function createTimeOutEvent(dateStamp) {
+    // Function to create a time-out event
+    function createTimeOutEvent(employeeRecord, dateStamp) {
     let [date, hour] = dateStamp.split(' ');
-    
-    this.timeOutEvents.push({
-        type: "TimeOut",
-        hour: parseInt(hour, 10),
-        date: date
+    employeeRecord.timeOutEvents.push({
+    type: "TimeOut",
+    hour: parseInt(hour, 10),
+    date: date
     });
-    
-    return this;
+    return employeeRecord;
     }
     
     // Function to calculate hours worked on a specific date
-    function hoursWorkedOnDate(date) {
-    let timeIn = this.timeInEvents.find(event => event.date === date);
-    let timeOut = this.timeOutEvents.find(event => event.date === date);
-    
-    return (timeOut.hour - timeIn.hour) / 100;
+    function hoursWorkedOnDate(employeeRecord, date) {
+    let timeInEvent = employeeRecord.timeInEvents.find(event => event.date === date);
+    let timeOutEvent = employeeRecord.timeOutEvents.find(event => event.date === date);
+    return (timeOutEvent.hour - timeInEvent.hour) / 100;
     }
     
     // Function to calculate wages earned on a specific date
-    function wagesEarnedOnDate(date) {
-    let hoursWorked = hoursWorkedOnDate.call(this, date);
-    return hoursWorked * this.payPerHour;
+    function wagesEarnedOnDate(employeeRecord, date) {
+    let hoursWorked = hoursWorkedOnDate(employeeRecord, date);
+    return hoursWorked * employeeRecord.payPerHour;
     }
     
-    // Provided function to calculate total wages for an employee
-    const allWagesFor = function () {
-    const eligibleDates = this.timeInEvents.map(function (e) {
-    return e.date;
-    });
-    
-    const payable = eligibleDates.reduce(function (memo, d) {
-        return memo + wagesEarnedOnDate.call(this, d);
-    }.bind(this), 0); // bind(this) ensures the correct 'this' context
-    
-    return payable;
-    };
-    
-    // Function to find an employee by their first name
-    function findEmployeeByFirstName(srcArray, firstName) {
-    return srcArray.find(employee => employee.firstName === firstName);
+    // Function to calculate all wages for an employee
+    function allWagesFor(employeeRecord) {
+    let datesWorked = employeeRecord.timeInEvents.map(event => event.date);
+    return datesWorked.reduce((totalWages, date) => totalWages + wagesEarnedOnDate(employeeRecord, date), 0);
     }
     
-    // Function to calculate total payroll for all employees
-    function calculatePayroll(employees) {
-    return employees.reduce((total, employee) => {
-    return total + allWagesFor.call(employee);
-    }, 0);
+    // Function to calculate payroll for all employees
+    function calculatePayroll(employeeRecords) {
+    return employeeRecords.reduce((totalPayroll, record) => totalPayroll + allWagesFor(record), 0);
     }
